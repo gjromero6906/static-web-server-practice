1bc06d95cb1acab60f42438f1ae5611791086751
gjromero6906 <gjr6906@gmail.com>
2026-03-05 14:27:43 -0500

    need to fix /

--- server/server.js
+++ server/server.js
@@ -8,8 +8,29 @@ const pathToFrontend = path.join(__dirname, '../frontend/dist');
 // generate middleware using the file path
 const serveStatic = express.static(pathToFrontend);
 
-// Register the serveStatic middleware before the remaining controllers
+const logRoutes = (req, res, next) => {
+  const time = new Date().toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next(); // pass to the next handler
+};
+
 app.use(serveStatic);
+
+app.use(logRoutes);
+const serveHTML = (req, res, next) => {
+  res.send('<h1>Welcome to the API</h1>');
+};
+
+const serveHello = (req, res, next) => {
+  res.send({ message: 'hello!' });
+};
+
+const serve404 = (req, res, next) => {
+  res.status(404).send({ error: `Not found: ${req.originalUrl}` });
+}
+app.get('/', serveHTML);
+app.get('/api/hello', serveHello);
+app.use(serve404);
 
 // other controllers 
 app.listen(PORT, () => {
