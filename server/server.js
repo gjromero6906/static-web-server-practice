bf048407988e1b5eb9f207ccc693968f72126208
gjromero6906 <gjr6906@gmail.com>
2026-03-05 13:31:07 -0500

    finished?

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 8080;
+// the file path to the dist directory
+const pathToFrontend = path.join(__dirname, '../frontend/dist');
+
+// generate middleware using the file path
+const serveStatic = express.static(pathToFrontend);
+
+// Register the serveStatic middleware before the remaining controllers
+app.use(serveStatic);
+
+// other controllers 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
